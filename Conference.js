dc8ddf864d3a5202e87624141ce7b80b940ef6d8
BreeBot <bree.merriweather@gmail.com>
2021-06-11 20:26:01 -0700

    js cpnference problem

---
+++ Conference.js
@@ -0,0 +1,11 @@
+class Conference {
+    constructor(name, attendees) {
+    this.name = name,
+    this.maxAttendees = attendees,
+    this.attendees = [],
+    this.sessions = [],
+    }
+}
+
+
+export default Conference
---
+++ test.js
@@ -0,0 +1,17 @@
+import Conference from "./Conference.js"
+
+describe("A conference", () => {
+    let conference, () => {
+        expect(Conference)
+    }
+})
+
+
+
+
+// A conference has a name defined when it is created
+
+
+// A conference has a maximum number of registrants defined when it is created
+// A conference starts with a blank list of attendees
+// A conference starts with a blank list of sessions
